3a9d0ea099afe03be61c27d8d9cf428b54a402ca
Jasmine <jasmine_singh1992@hotmail.com>
2020-03-04 23:18:54 +0000

    Remove ignored files

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -45,10 +45,11 @@ pool
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
+WHERE cohorts.name LIKE $1'
+LIMIT $2;
 `
   )
+  const values = [`%${cohortName}%`, limit]; 
   .then(res => {
     res.rows.forEach(user => {
       console.log(
