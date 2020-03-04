4881083b89403cf3341073175174110260cff712
Jasmine <jasmine_singh1992@hotmail.com>
2020-03-04 22:25:17 +0000

    Remove ignored files

--- 4_queries/12_Teacher_name_and_assistances.sql
+++ 4_queries/12_Teacher_name_and_assistances.sql
@@ -1,4 +1,4 @@
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort, COUNT(assistance_requests.*) as total_assistances
+SELECT teachers.name as teacher, cohorts.name as cohort, COUNT(assistance_requests.*) as total_assistances
 FROM teachers
   JOIN assistance_requests ON teachers.id = teacher_id
   JOIN students ON students.id = student_id
